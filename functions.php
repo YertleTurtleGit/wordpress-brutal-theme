bb18be4eaf3eb7dd4f8427e0180d6e92cb9f6900
YertleTurtleGit <34098804+YertleTurtleGit@users.noreply.github.com>
2024-04-26 00:01:08 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,3 @@
 <?php
-function remove_from_admin_bar($wp_admin_bar) {
-    if ( ! is_admin() ) { 
-        // WordPress Core Items (uncomment to remove)
-        //$wp_admin_bar->remove_node('updates');
-        $wp_admin_bar->remove_node('comments');
-        $wp_admin_bar->remove_node('new-content');
-        //$wp_admin_bar->remove_node('wp-logo');
-        //$wp_admin_bar->remove_node('site-name');
-        //$wp_admin_bar->remove_node('my-account');
-        //$wp_admin_bar->remove_node('search');
-        //$wp_admin_bar->remove_node('customize');
-    }
-}
-add_action('admin_bar_menu', 'remove_from_admin_bar', 999);
+
 ?>
