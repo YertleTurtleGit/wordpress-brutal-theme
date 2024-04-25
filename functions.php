ea9fcbd7433f74b82e7fa3127689fdad927239c1
YertleTurtleGit <34098804+YertleTurtleGit@users.noreply.github.com>
2024-04-25 23:57:08 +0200

    fix

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
-function remove_comments(){
-        global $wp_admin_bar;
-        $wp_admin_bar->remove_menu('comments');
+function prefix_remove_comments_tl() {
+        remove_menu_page( ‘edit-comments.php’ );
 }
-add_action( 'wp_before_admin_bar_render', 'remove_comments' );
+
+add_action( ‘admin_menu’, ‘prefix_remove_comments_tl’ );
