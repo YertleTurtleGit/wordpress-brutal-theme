fbbe75c60d7ae447df13c720bc552c35a7bea6fc
YertleTurtleGit <github@galzone.de>
2024-10-06 13:47:53 +0200

    rock

--- render.js
+++ render.js
@@ -1,13 +1,8 @@
-import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
-import * as THREE from 'three';
+import { DropInViewer } from "@mkkellogg/gaussian-splats-3d";
+import * as THREE from "three";
 
-const templateUrl = '<?= get_bloginfo("template_url"); ?>';
-console.log(templateUrl);
-
-console.log("nlabla");
-
-const renderWidth = 800;
-const renderHeight = 600;
+const renderWidth = 400;
+const renderHeight = 250;
 
 const rootElement = document.getElementById("canvas-div");
 rootElement.style.width = renderWidth + "px";
@@ -23,31 +18,35 @@ const camera = new THREE.PerspectiveCamera(
   65,
   renderWidth / renderHeight,
   0.1,
-  500
+  2000
 );
-camera.position.copy(new THREE.Vector3().fromArray([-1, -4, 6]));
-camera.up = new THREE.Vector3().fromArray([0, -1, -0.6]).normalize();
-camera.lookAt(new THREE.Vector3().fromArray([0, 4, -0]));
+camera.position.copy(new THREE.Vector3().fromArray([5 / 2, 3 / 2, 2 / 2]));
+camera.up = new THREE.Vector3().fromArray([0, -1, 0]).normalize();
+camera.lookAt(new THREE.Vector3().fromArray([0, 0, 0]));
+camera.updateMatrix();
+camera.updateMatrixWorld();
+camera.updateProjectionMatrix();
 
-const viewer = new GaussianSplats3D.Viewer({
-  selfDrivenMode: false,
-  renderer: renderer,
-  camera: camera,
-  useBuiltInControls: false,
-  ignoreDevicePixelRatio: false,
+const threeScene = new THREE.Scene();
+
+const boxGeometry = new THREE.BoxGeometry(1 / 3, 1 / 3, 1 / 3);
+const boxMesh = new THREE.Mesh(
+  boxGeometry,
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+boxMesh.position.set(0, 0, 0);
+threeScene.add(boxMesh);
+
+const viewer = new DropInViewer({
   gpuAcceleratedSort: true,
-  halfPrecisionCovariancesOnGPU: true,
-  sharedMemoryForWorkers: true,
-  integerBasedSort: true,
-  dynamicScene: false,
-  webXRMode: GaussianSplats3D.WebXRMode.None,
-  renderMode: GaussianSplats3D.RenderMode.OnChange,
-  sceneRevealMode: GaussianSplats3D.SceneRevealMode.Instant,
-  antialiased: false,
-  focalAdjustment: 1.0,
-  logLevel: GaussianSplats3D.LogLevel.None,
-  sphericalHarmonicsDegree: 0,
+  sharedMemoryForWorkers: false,
 });
-viewer.addSplatScene("test.ply").then(() => {
-  requestAnimationFrame(update);
-});
+threeScene.add(viewer);
+function render() {
+  console.log("render");
+  viewer.updateSplatMesh();
+  renderer.render(threeScene, camera);
+  requestAnimationFrame(render);
+}
+
+viewer.addSplatScene(THEME_PATH + "/rock.ply").then(render);
