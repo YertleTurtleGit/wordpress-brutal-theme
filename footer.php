48c60e4b84a19f082c5ca8525ff5f4c9026387da
YertleTurtleGit <34098804+YertleTurtleGit@users.noreply.github.com>
2024-11-26 09:54:12 +0100

    Tmp fix click (#4)
    * center small
    
    * fix click

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
+
+<div id="canvas-div"></div>
+
+<script type="text/javascript">
+    const THEME_PATH = "<?php echo get_bloginfo('template_directory'); ?>/";
+</script>
+
+<script
+    type="module"
+    src="<?php echo get_bloginfo('template_directory'); ?>/dist/bundle.js">
+</script>
+
 <?php wp_footer(); ?>
 </body>
 
