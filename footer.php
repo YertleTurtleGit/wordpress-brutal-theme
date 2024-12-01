5e8a03e09f1ce30e8536715aebbe22cb70664f0f
YertleTurtleGit <github@galzone.de>
2024-12-01 11:10:11 +0100

    impress menu

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
 
 <div id="canvas-div"></div>
 
+<ul id="impress-menu">
+    <li><a href="<?php echo get_home_url(); ?>/impressum">Impressum</a></li>
+    <li><a href="<?php echo get_home_url(); ?>/datenschutz">Datenschutz</a></li>
+</ul>
+
 <script type="text/javascript">
     const THEME_PATH = "<?php echo get_bloginfo('template_directory'); ?>/";
 </script>
